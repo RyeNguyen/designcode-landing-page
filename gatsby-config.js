4a74b1a7a75068b05e083f4a75b4d9d94104363f
Minh Nguyen <minhiq149@gmail.com>
2022-06-22 15:15:45 +0700

    Init project and setup React, Gatsby and Styled Components

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    siteMetadata: {
+        title: `Design+Code Landing Page`,
+        description: `Don’t skip design. Learn design and code, by building real apps with React and Swift. Complete courses about the best tools.`,
+        author: `@rye__principe__`,
+    },
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/static/images`,
+            },
+        },
+        `gatsby-plugin-styled-components`
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
+}
